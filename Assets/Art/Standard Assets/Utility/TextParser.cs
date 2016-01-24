e2c5d1b98bd999d46fd59e816ef658fd66cd35c0
Pablo Leon-Luna <paleonluna@gmail.com>
2016-01-23 19:08:47 -0800

    added in text parser for conversations needs to be set up still

---
+++ Assets/Art/Standard Assets/Utility/TextParser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class TextParser {
+
+
+	public static List<pointOfSail> Parse(TextAsset csvString){
+		//get the array of lines
+		List<pointOfSail> arrayPointOfSail = new List<pointOfSail>();
+		string[] arrayOfLines = csvString.ToString().Split ("\n" [0]);
+
+		//take each line, split by comma, and then populate list at that index
+		for (int i=0; i<arrayOfLines.Length; i++) {
+			string[] arrayOfStrings = arrayOfLines[i].Split(","[0]);
+			pointOfSail tempPOS =  new pointOfSail(arrayOfStrings[0],float.Parse(arrayOfStrings[1]));
+			arrayPointOfSail.Add(tempPOS);
+		}
+		return arrayPointOfSail;
+	}
+}
+
+public class Conversation () {
+
+}
