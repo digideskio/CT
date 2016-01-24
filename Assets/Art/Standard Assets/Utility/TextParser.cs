028e48477f555bb98653db89a7adf4d416df5935
Pablo Leon-Luna <paleonluna@gmail.com>
2016-01-23 19:09:32 -0800

    textparser

--- Assets/Art/Standard Assets/Utility/TextParser.cs
+++ Assets/Art/Standard Assets/Utility/TextParser.cs
@@ -5,21 +5,21 @@ using System.Collections.Generic;
 public static class TextParser {
 
 
-	public static List<pointOfSail> Parse(TextAsset csvString){
+	public static List<Conversation> Parse(TextAsset csvString){
 		//get the array of lines
-		List<pointOfSail> arrayPointOfSail = new List<pointOfSail>();
+		List<Conversation> arrayPointOfSail = new List<Conversation>();
 		string[] arrayOfLines = csvString.ToString().Split ("\n" [0]);
 
 		//take each line, split by comma, and then populate list at that index
 		for (int i=0; i<arrayOfLines.Length; i++) {
 			string[] arrayOfStrings = arrayOfLines[i].Split(","[0]);
-			pointOfSail tempPOS =  new pointOfSail(arrayOfStrings[0],float.Parse(arrayOfStrings[1]));
+			Conversation tempPOS =  new Conversation(arrayOfStrings[0],float.Parse(arrayOfStrings[1]));
 			arrayPointOfSail.Add(tempPOS);
 		}
 		return arrayPointOfSail;
 	}
 }
 
-public class Conversation () {
+public class Conversation {
 
 }
