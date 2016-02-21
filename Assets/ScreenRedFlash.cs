35cd011874edc34047d70ad49090198d0f0e0614
paleonluna <paleonluna@gmail.com>
2016-02-20 18:58:16 -0800

    carpg13

---
+++ Assets/ScreenRedFlash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRedFlash : MonoBehaviour {
+
+
+	public float opacity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public static ScreenRedFlash s_instance;
+	void Awake () {
+		if (s_instance == null) {
+			s_instance = this;
+		}
+		else {
+			Destroy(gameObject);
+		}	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
